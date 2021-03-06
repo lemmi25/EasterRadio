2658e75fd134e32b5713c6db595f20a531060129
Moritz Bösenberg <moritz.boesenberg@outlook.com>
2021-03-06 21:33:49 +0100

    added I2S example

---
+++ ESP_32_TEST/src/main.cpp
@@ -0,0 +1,74 @@
+#include "Arduino.h"
+#include "WiFi.h"
+#include "Wire.h"
+
+#include "SD.h"
+#include "FS.h"
+#include "Audio.h"
+
+// Digital I/O used
+#define I2S_DOUT      12
+#define I2S_BCLK      13
+#define I2S_LRC       14
+
+Audio audio;
+
+String ssid =     "iphone";
+String password = "Lutschpuffer";
+
+void setup() {  
+    Serial.begin(115200);
+
+    WiFi.disconnect();
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid.c_str(), password.c_str());
+    while (WiFi.status() != WL_CONNECTED) delay(1500);
+    audio.setPinout(I2S_BCLK, I2S_LRC, I2S_DOUT);
+    audio.setVolume(12); // 0...21
+
+//    audio.connecttoFS(SD, "/320k_test.mp3");
+//    audio.connecttohost("http://www.wdr.de/wdrlive/media/einslive.m3u");
+//    audio.connecttohost("http://macslons-irish-pub-radio.com/media.asx");
+//    audio.connecttohost("http://mp3.ffh.de/radioffh/hqlivestream.aac"); //  128k aac
+      audio.connecttohost("https://st01.sslstream.dlf.de/dlf/01/128/mp3/stream.mp3"); //  128k mp3
+//    audio.connecttohost("https://github.com/schreibfaul1/ESP32-audioI2S/raw/master/additional_info/Testfiles/sample1.m4a"); // m4a
+   // audio.connecttohost("https://github.com/schreibfaul1/ESP32-audioI2S/raw/master/additional_info/Testfiles/test_16bit_stereo.wav"); // wav
+    //audio.connecttospeech("Hallo Henrike ich kann mit dir über I 2 S reden Haaa Haaa haaaaaa", "de");
+}
+
+void loop()
+{
+    audio.loop();
+}
+
+// optional
+void audio_info(const char *info){
+    Serial.print("info        "); Serial.println(info);
+}
+void audio_id3data(const char *info){  //id3 metadata
+    Serial.print("id3data     ");Serial.println(info);
+}
+void audio_eof_mp3(const char *info){  //end of file
+    Serial.print("eof_mp3     ");Serial.println(info);
+}
+void audio_showstation(const char *info){
+    Serial.print("station     ");Serial.println(info);
+}
+void audio_showstreamtitle(const char *info){
+    Serial.print("streamtitle ");Serial.println(info);
+}
+void audio_bitrate(const char *info){
+    Serial.print("bitrate     ");Serial.println(info);
+}
+void audio_commercial(const char *info){  //duration in sec
+    Serial.print("commercial  ");Serial.println(info);
+}
+void audio_icyurl(const char *info){  //homepage
+    Serial.print("icyurl      ");Serial.println(info);
+}
+void audio_lasthost(const char *info){  //stream URL played
+    Serial.print("lasthost    ");Serial.println(info);
+}
+void audio_eof_speech(const char *info){
+    Serial.print("eof_speech  ");Serial.println(info);
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# EasterRadio
-Repo for our EasterRadio
+# EasterRadio I2S
+
+Simple Example for working I2S working. Webradio and other stuff.
